e3c65da4e0c39760815f0d7cc080e1fb416a44c2
Thanh-Quy Nguyen <nguyen.tq@hotmail.fr>
2019-10-16 16:33:31 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+	<div id="app">
+		<todo-list v-bind:todos="todos"></todo-list>
+	</div>
+</template>
+
+<script>
+import TodoList from "./components/TodoList";
+
+export default {
+	name: "App",
+	components: {
+		TodoList
+	},
+	data() {
+		return {
+			todos: [
+				{
+					title: "Todo A",
+					project: "Project A",
+					done: false
+				},
+				{
+					title: "Todo B",
+					project: "Project B",
+					done: true
+				},
+				{
+					title: "Todo C",
+					project: "Project C",
+					done: false
+				},
+				{
+					title: "Todo D",
+					project: "Project D",
+					done: false
+				}
+			]
+		};
+	}
+};
+</script>
+
+<style>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<!-- JavaScript expressions in Vue are enclosed in double curly brackets. -->
+		<p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+		<p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+		<todo v-for="todo in todos" v-bind:todo="todo"></todo>
+	</div>
+</template>
+
+<script type = "text/javascript" >
+import Todo from "./Todo";
+export default {
+	props: ["todos"],
+	components: {
+		Todo
+	}
+};
+</script>
+
+<style>
+</style>
